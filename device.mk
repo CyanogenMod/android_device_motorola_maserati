0fc3c3f4c0cc08a1a2fc99c0ae26abb497720eec
dhacker29 <davidhackerdvm@gmail.com>
2013-07-15 23:33:32 -0500

    Add extract files
    Change-Id: I06f760cafb7c49af9bd1027cdf66d73e1ce7476b

--- device.mk
+++ device.mk
@@ -52,4 +52,5 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/motorola/maserati/maserati-vendor.mk)
-$(call inherit-product-if-exists, vendor/motorola/omap4-common/common-vendor-pvr.mk)
+$(call inherit-product-if-exists, vendor/motorola/omap4-common/omap4-common-imgtec-vendor-blobs.mk)
+$(call inherit-product-if-exists, vendor/motorola/omap4-common/omap4-common-lte-vendor-blobs.mk)
